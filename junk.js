b2b9e793d687191d319c5ccfb92e2deca8ae4f9f
Philani Mxathule <3911755@myuwc.ac.za>
2022-11-17 11:51:37 +0200

    progres

---
+++ junk.js
@@ -0,0 +1,21 @@
+// function App() {
+
+//     const title = "Welcome to the new blog";
+//     const likes = 50;
+//     const num = 1;
+  
+//     const link = "https://www.google.com"
+//     return <div className="App">
+//       <div className="content">
+//         <h1>{ title }</h1>
+//         <p>Liked {likes} times</p>
+//         <p> { 10 }</p>
+//         <p>{"Hello, ninjas"}</p>
+//         <p>{[1, 2, 3, 4, 5]}</p>
+//         <p>{ Math.random() * 10}</p>
+//         <a href={link}>Google</a>
+//         <p>{console.log(num)}</p>
+  
+//       </div>
+//     </div>;
+//   }
--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ const Navbar = () => {
         <nav className="navbar">
             <h1>Learning ReactJS</h1>
             <div className="links">
-                <a href="/">Home</a>
+                <a href="/">Home</a>|| 
                 <a href="/newblog">New Blog</a>
             </div>
         </nav>
