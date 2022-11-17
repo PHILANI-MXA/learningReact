331ba9f6993eab7b5f7dfcf25a8667c19005adea
Philani Mxathule <3911755@myuwc.ac.za>
2022-11-17 11:32:03 +0200

    web app

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Learning ReactJS</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/newblog">New Blog</a>
+            </div>
+        </nav>
+     );
+}
+
+export default Navbar
